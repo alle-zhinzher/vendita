7b790cc32660740d5bae0252cfa389fda5b3fbab
alle-zhinzher <sanches2000009@gmail.com>
2019-07-25 01:50:16 +0300

    Create paginator

--- vendita/frontend/src/components/pageComponents/bookList/BookList.js
+++ vendita/frontend/src/components/pageComponents/bookList/BookList.js
@@ -2,19 +2,71 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Cart from './Cart';
+import css from './Books.css'
+
 
 class BookList extends Component {
     static propTypes = {
         sorted_books: PropTypes.array.isRequired,
     }
 
+    componentWillMount() {
+        this.setState({
+            active_page: 1,
+            check_lenght: this.props.sorted_books.length
+        })
+    }
+
+    new_paginator = () => {
+        this.setState({
+            active_page: 1,
+            check_lenght: this.props.sorted_books.length
+        })
+    }
+
+    change_page(page) {
+        this.setState({
+            active_page: page
+        })
+    }
+    next_page() {
+        this.setState({
+            active_page: this.state.active_page + 1
+        })
+    }
+
+    prev_page() {
+        this.setState({
+            active_page: this.state.active_page - 1
+        })
+    }
+
+
+
     render() {
+        const page = this.state.active_page
+        const itemCount = this.props.sorted_books.length
         return (
-            <section className="content-section book-list" >
-                {this.props.sorted_books.map((book, index = 0) =>
-                    <Cart key={book.id} book={book} index={index++} />
-                )}
-            </section>
+            <section className="content-section" >
+                <section className="book-list">
+                    {this.props.sorted_books.map((book, index = 1) =>
+                        <Cart key={book.id} book={book} index={index++} active_page={this.state.active_page} />
+                    )}
+                </section>
+                <section className="paginator">
+                    <button className={page === 1 ? "disable" : "paginator-button"} onClick={() => this.prev_page()}>&#x2039;</button>
+
+                    <button className={page === 1 ? "disable" : "paginator-button"} onClick={() => this.change_page(this.state.active_page - 1)}>
+                        {this.state.active_page - 1}</button>
+                    <button className="paginator-button paginator-active">
+                        {this.state.check_lenght === itemCount ? this.state.active_page : this.new_paginator()}</button>
+                    <button className={page >= itemCount / 4 ? "disable" : "paginator-button"} onClick={() => this.change_page(this.state.active_page + 1)}>
+                        {this.state.active_page + 1}</button>
+
+                    <button className={page >= itemCount / 4 ? "disable" : "paginator-button"} onClick={() => this.next_page()}>&#x203a;</button>
+                </section >
+
+            </section >
         )
     }
 }
--- vendita/frontend/src/components/pageComponents/bookList/Cart.js
+++ vendita/frontend/src/components/pageComponents/bookList/Cart.js
@@ -2,12 +2,30 @@ import React, { Component } from 'react'
 
 class Cart extends Component {
     render() {
+
+        const max = this.props.active_page * 4;
+        const min = max - 4;
+
+        const page_status = this.props.index >= min & this.props.index < max ?
+            "active-page book-cart"
+            :
+            "disable"
+
         return (
-            <div>
-                <h2>{this.props.book.name}</h2>
-                <h2>{this.props.book.author}</h2>
-                <h2>{this.props.book.cost}</h2>
-                <h2>{this.props.book.pages}</h2>
+            <div className={page_status}>
+                <div className="book-cart-image">
+                    <img src={this.props.book.image} alt="book image" />
+                </div>
+                <div className="book-name">
+                    <h3>{this.props.book.name}</h3>
+                    <h4>{this.props.book.author}</h4>
+                </div>
+                <div className="book-discripion">
+                    <p>{this.props.book.discrition}</p>
+                    <h4>{this.props.book.pages}- pages</h4>
+                </div>
+
+                <h2>Price: <span className="book-price">{this.props.book.cost}$</span></h2>
 
             </div>
         )
