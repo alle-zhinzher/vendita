1d5a0c0fda67c1b30b56424612f65e1ecd1799dc
alle-zhinzher <sanches2000009@gmail.com>
2019-07-29 23:48:53 +0300

    Refactoring Login & Logout

--- vendita/frontend/src/actions/auth.js
+++ vendita/frontend/src/actions/auth.js
@@ -14,26 +14,8 @@ import {
 
 // CHECK TOKEN & LOAD USER
 export const loadUser = () => (dispatch, getState) => {
-    // User Loading
-    dispatch({ type: USER_LOADING });
-
-    // Get token from state
-    const token = getState().authReducer.token;
-
-    // Headers
-    const config = {
-        headers: {
-            "Content-Type": "application/json"
-        }
-    };
-
-    // If token, add to headers config
-    if (token) {
-        config.headers["Authorization"] = `Token ${token}`;
-    }
-
     axios
-        .get("/api/auth/user", config)
+        .get("/api/auth/user", tokenConfig(getState))
         .then(res => {
             dispatch({
                 type: USER_LOADED,
@@ -57,7 +39,6 @@ export const login = (username, password) => dispatch => {
             "Content-Type": "application/json"
         }
     };
-
     // Request Body
     const body = JSON.stringify({ username, password });
 
@@ -80,9 +61,25 @@ export const login = (username, password) => dispatch => {
 
 // CHECK TOKEN & LOAD USER
 export const logout = () => (dispatch, getState) => {
+    axios
+        .post("/api/auth/logout/", null, tokenConfig(getState))
+        .then(res => {
+            dispatch({
+                type: LOGOUT_SUCCESS,
+            });
+        })
+        .catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status));
+        });
+};
 
+
+
+
+// Setup config with token - helper function
+export const tokenConfig = getState => {
     // Get token from state
-    const token = getState().authReducer.token;
+    const token = getState().auth.token;
 
     // Headers
     const config = {
@@ -96,14 +93,5 @@ export const logout = () => (dispatch, getState) => {
         config.headers["Authorization"] = `Token ${token}`;
     }
 
-    axios
-        .post("/api/auth/logout/", null, config)
-        .then(res => {
-            dispatch({
-                type: LOGOUT_SUCCESS,
-            });
-        })
-        .catch(err => {
-            dispatch(returnErrors(err.response.data, err.response.status));
-        });
+    return config;
 };
